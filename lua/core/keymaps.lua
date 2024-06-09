0aa9db75d6c65cc72b940003619a5bb5f1bd1f24
William Lempinen <wlempin@gmail.com>
2024-06-09 17:29:27 +0300

    terminal config

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -4,6 +4,9 @@ vim.keymap.set("n", "<Left>", "<C-w>h", { noremap = true, silent = true })
 vim.keymap.set("n", "<Right>", "<C-w>l", { noremap = true, silent = true })
 
 
+-- terminal
+vim.keymap.set("n", "<C-t>", ":belowright split | resize 10 | terminal<CR>", { noremap = true, silent = true })
+vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { noremap = true, silent = true })
 
 
 

6918a936a08284d07f6aa3f203fc4b8a59aca007
William Lempinen <wlempin@gmail.com>
2024-07-01 19:29:56 +0300

    -

--- lua/options.lua
+++ lua/options.lua
@@ -8,10 +8,11 @@ vim.opt.laststatus = 2
 vim.opt.autowrite = true
 vim.opt.autoread = true
 vim.opt.mouse = "a"
-
+vim.opt.scrolloff = 8
+vim.opt.ruler = true
 vim.opt.wrap = true
 vim.opt.number = true
-
+vim.opt.autoindent = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.shiftround = true
@@ -19,4 +20,3 @@ vim.opt.expandtab = true
 
 vim.cmd([[ set noswapfile ]])
 vim.cmd([[ set termguicolors ]])
-
--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,32 +1,32 @@
 return {
-  "akinsho/toggleterm.nvim",
-  config = function()
-    require("toggleterm").setup({
-      size = 12,
-      open_mapping = false,
-      hide_numbers = true,
-      shade_filetypes = {},
-      direction = "horizontal",
-      close_on_exit = true,
-      shade_terminals = true,
-      shading_factor = 1,
-      shell = "zsh",
-      persist_size = true,
-      start_in_insert = false,
-      autochdir = true,
-    })
+	"akinsho/toggleterm.nvim",
+	config = function()
+		require("toggleterm").setup({
+			size = 12,
+			open_mapping = false,
+			hide_numbers = true,
+			shade_filetypes = {},
+			direction = "horizontal",
+			close_on_exit = true,
+			shade_terminals = true,
+			shading_factor = 1,
+			shell = vim.o.shell,
+			persist_size = true,
+			start_in_insert = false,
+			autochdir = true,
+		})
 
-    local map = vim.api.nvim_set_keymap
-    local opts = { noremap = true, silent = true }
+		local map = vim.api.nvim_set_keymap
+		local opts = { noremap = true, silent = true }
 
-    map("n", "<c-t>", "<cmd>1ToggleTerm<CR>", opts)
-    map("n", "<c-y>", "<cmd>2ToggleTerm<CR>", opts)
+		map("n", "<c-t>", "<cmd>1ToggleTerm<CR>", opts)
+		map("n", "<c-y>", "<cmd>2ToggleTerm<CR>", opts)
 
-    function _G.set_terminal_keymaps()
-      local term_opts = { noremap = true, silent = true }
-      vim.api.nvim_buf_set_keymap(0, "t", "<Esc>", [[<C-\><C-n>]], term_opts)
-    end
+		function _G.set_terminal_keymaps()
+			local term_opts = { noremap = true, silent = true }
+			vim.api.nvim_buf_set_keymap(0, "t", "<Esc>", [[<C-\><C-n>]], term_opts)
+		end
 
-    vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
-  end,
+		vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
+	end,
 }

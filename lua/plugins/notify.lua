12e67c8bb42df9182c9d7d9c2b841638587285d7
William Lempinen <wlempin@gmail.com>
2024-09-03 16:49:20 +0300

    -

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -1,13 +1,13 @@
 return {
-  "rcarriga/nvim-notify",
-  config = function()
-    vim.notify = require("notify")
-    require("notify").setup({
-      stages = "fade_in_slide_out",
-      timeout = 2500,
-      render = "compact",
-      level = "info",
-    })
-  end,
-  lazy = false,
+	"rcarriga/nvim-notify",
+	config = function()
+		vim.notify = require("notify")
+		require("notify").setup({
+			stages = "fade_in_slide_out",
+			timeout = 2500,
+			render = "compact",
+			level = "warn",
+		})
+	end,
+	lazy = false,
 }

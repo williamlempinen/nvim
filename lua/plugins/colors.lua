9a673fe4c6d1f2e422e696bd31283bd5ae7cf861
William Lempinen <wlempin@gmail.com>
2024-10-18 18:54:17 +0300

    -

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,9 +1,31 @@
 return {
-  "projekt0n/github-nvim-theme",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    require("github-theme").setup({})
-    vim.cmd("colorscheme github_dark_default")
-  end,
+  {
+    "Yazeed1s/minimal.nvim",
+  },
+  {
+    "killitar/obscure.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+
+  {
+    {
+      "vague2k/vague.nvim",
+      config = function()
+        require("vague").setup({
+          -- optional configuration here
+        })
+      end,
+    },
+  },
+  {
+    "projekt0n/github-nvim-theme",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("github-theme").setup({})
+      vim.cmd("colorscheme github_dark_default")
+    end,
+  },
 }

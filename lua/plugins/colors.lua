7cec1df51f6c70e62e6d3605282824c714a10dd3
William Lempinen <wlempin@gmail.com>
2024-10-19 15:57:46 +0300

    -

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,31 +1,24 @@
 return {
-  {
-    "Yazeed1s/minimal.nvim",
-  },
-  {
-    "killitar/obscure.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
-  },
-
-  {
-    {
-      "vague2k/vague.nvim",
-      config = function()
-        require("vague").setup({
-          -- optional configuration here
-        })
-      end,
-    },
-  },
-  {
-    "projekt0n/github-nvim-theme",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("github-theme").setup({})
-      vim.cmd("colorscheme github_dark_default")
-    end,
-  },
+	{ "rose-pine/neovim", name = "rose-pine" },
+	{ "kvrohit/rasmus.nvim" },
+	{
+		"killitar/obscure.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+	},
+	{
+		"vague2k/vague.nvim",
+		config = function()
+			require("vague").setup({})
+		end,
+	},
+	{
+		"projekt0n/github-nvim-theme",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("github-theme").setup({})
+		end,
+	},
 }

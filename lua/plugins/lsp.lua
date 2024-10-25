a45634b89d1e1060a2c80c21adc0908ad235ed86
William Lempinen <wislem@utu.fi>
2024-10-25 15:26:38 +0300

    -

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,110 +1,126 @@
 return {
-	{
-		"hrsh7th/nvim-cmp",
-		event = "InsertEnter",
-		dependencies = {
-			{ "L3MON4D3/LuaSnip" },
-			{ "hrsh7th/cmp-buffer" },
-			{ "hrsh7th/cmp-path" },
-			{ "hrsh7th/cmp-cmdline" },
-		},
-		config = function()
-			local cmp = require("cmp")
+  {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+      { "L3MON4D3/LuaSnip" },
+      { "hrsh7th/cmp-buffer" },
+      { "hrsh7th/cmp-path" },
+      { "hrsh7th/cmp-cmdline" },
+    },
+    config = function()
+      local cmp = require("cmp")
 
-			cmp.setup({
-				window = {
-					completion = cmp.config.window.bordered(),
-					documentation = cmp.config.window.bordered(),
-				},
-				mapping = cmp.mapping.preset.insert({
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
-					["<C-u>"] = cmp.mapping.scroll_docs(-4),
-					["<C-d>"] = cmp.mapping.scroll_docs(4),
-				}),
-				snippet = {
-					expand = function(args)
-						vim.snippet.expand(args.body)
-					end,
-				},
-				sources = {
-					{ name = "nvim_lsp" },
-				},
-			})
-		end,
-	},
-	{
-		"williamboman/mason.nvim",
-		opts = {
-			ensure_installed = {
-				"lua_ls",
-				"tsserver",
-				"html",
-				"cssls",
-				"tailwindcss",
-				"prismals",
-				"pyright",
-			},
-		},
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"tsserver",
-					"html",
-					"cssls",
-					"tailwindcss",
-					"prismals",
-					"pyright",
-				},
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		cmd = { "LspInfo", "LspInstall", "LspStart" },
-		event = { "BufReadPre", "BufNewFile" },
-		dependencies = {
-			{ "hrsh7th/cmp-nvim-lsp" },
-			{ "williamboman/mason.nvim" },
-			{ "williamboman/mason-lspconfig.nvim" },
-		},
-		config = function()
-			local lspconfig = require("lspconfig")
-			local capabilities =
-				require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
+      cmp.setup({
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-d>"] = cmp.mapping.scroll_docs(4),
+        }),
+        snippet = {
+          expand = function(args)
+            vim.snippet.expand(args.body)
+          end,
+        },
+        sources = {
+          { name = "nvim_lsp" },
+        },
+      })
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua_ls",
+        "tsserver",
+        "html",
+        "cssls",
+        "tailwindcss",
+        "prismals",
+        "pyright",
+      },
+    },
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "tsserver",
+          "html",
+          "cssls",
+          "tailwindcss",
+          "prismals",
+          "pyright",
+        },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    cmd = { "LspInfo", "LspInstall", "LspStart" },
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      { "hrsh7th/cmp-nvim-lsp" },
+      { "williamboman/mason.nvim" },
+      { "williamboman/mason-lspconfig.nvim" },
+    },
+    config = function()
+      local lspconfig = require("lspconfig")
+      local capabilities =
+          require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-			local on_attach = function(client, bufnr)
-				local opts = { buffer = bufnr }
-				vim.keymap.set("n", "P", vim.lsp.buf.hover, opts)
-				vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-				vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-				vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-				vim.keymap.set("n", "go", vim.lsp.buf.type_definition, opts)
-				vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-				vim.keymap.set("n", "gs", vim.lsp.buf.signature_help, opts)
+      local on_attach = function(client, bufnr)
+        local opts = { buffer = bufnr }
+        vim.keymap.set("n", "P", vim.lsp.buf.hover, opts)
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+        vim.keymap.set("n", "go", vim.lsp.buf.type_definition, opts)
+        vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+        vim.keymap.set("n", "gs", vim.lsp.buf.signature_help, opts)
 
-				vim.diagnostic.config({
-					virtual_text = {
-						severity = { min = vim.diagnostic.severity.WARN },
-					},
-					signs = true,
-					underline = true,
-				})
-			end
+        vim.diagnostic.config({
+          virtual_text = {
+            severity = { min = vim.diagnostic.severity.WARN },
+          },
+          signs = true,
+          underline = true,
+        })
+      end
 
-			-- eslint should be added here sometime, return some error
-			local servers = { "tsserver", "tailwindcss", "lua_ls", "html", "cssls", "pyright" }
+      -- eslint should be added here sometime, return some error
+      local servers = { "tsserver", "tailwindcss", "lua_ls", "html", "cssls", "pyright" }
 
-			for _, server in ipairs(servers) do
-				lspconfig[server].setup({
-					on_attach = on_attach,
-					capabilities = capabilities,
-				})
-			end
-		end,
-	},
+      for _, server in ipairs(servers) do
+        lspconfig[server].setup({
+          on_attach = on_attach,
+          capabilities = capabilities,
+        })
+      end
+    end,
+  },
+  {
+    'simrat39/rust-tools.nvim',
+    config = function()
+      local rt = require('rust-tools')
+      rt.setup({
+        server = {
+          on_attach = function(_, bufnr)
+            -- Hover actions
+            vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+            -- Code action groups
+            vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+          end,
+        },
+      })
+    end
+  }
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,26 +1,28 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		config = function()
-			local config = require("nvim-treesitter.configs")
-			config.setup({
-				ensure_installed = {
-					"c",
-					"lua",
-					"vim",
-					"html",
-					"java",
-					"javascript",
-					"typescript",
-					"tsx",
-					"json",
-					"python",
-				},
-				auto_install = true,
-				highlight = { enable = true },
-				indent = { enable = true },
-			})
-		end,
-	},
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      local config = require("nvim-treesitter.configs")
+      config.setup({
+        ensure_installed = {
+          "c",
+          "lua",
+          "vim",
+          "html",
+          "java",
+          "javascript",
+          "typescript",
+          "tsx",
+          "json",
+          "python",
+          "rust",
+          "toml"
+        },
+        auto_install = true,
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end,
+  },
 }

b989d50731b36b12ae611d029e2d7b1923f082b6
William Lempinen <wislem@utu.fi>
2025-02-18 15:49:16 +0200

    workaround for rust error

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -101,6 +101,17 @@ return {
 				})
 			end
 
+			-- rust_analyzer workaround for 32802 error
+			for _, method in ipairs({ "textDocument/diagnostic", "workspace/diagnostic" }) do
+				local default_diagnostic_handler = vim.lsp.handlers[method]
+				vim.lsp.handlers[method] = function(err, result, context, config)
+					if err ~= nil and err.code == -32802 then
+						return
+					end
+					return default_diagnostic_handler(err, result, context, config)
+				end
+			end
+
 			vim.api.nvim_create_autocmd("ColorScheme", {
 				callback = function()
 					vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#ffffff", bg = "" })

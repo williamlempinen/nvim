9738cb5bd806f9ce15a1deb8922d469ae4056411
William Lempinen <wislem@utu.fi>
2025-04-23 13:11:24 +0300

    back to left

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -23,7 +23,7 @@ return {
 				centralize_selection = false,
 				cursorline = true,
 				debounce_delay = 15,
-				side = "right",
+				side = "left",
 				preserve_window_proportions = false,
 				number = false,
 				relativenumber = false,
